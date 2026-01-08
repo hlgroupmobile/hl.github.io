607a0e25ff1be3b1d47cd6f95e0e90181cba6ab5
hlgroupmobile <hlgroupmobile@gmail.com>
2026-01-08 15:14:26 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function openLink(url) {
+  if (url === "#") {
+    alert("链接尚未配置\n아직 연결되지 않았습니다");
+  } else {
+    window.open(url, "_blank");
+  }
+}
+
+function comingSoon() {
+  alert("准备中功能\n준비 중입니다");
+}
+
